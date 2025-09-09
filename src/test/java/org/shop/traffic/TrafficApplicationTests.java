82a1e73979b9d213496994a9de30c7df55df4481
Amelia <nawrotamelia3@gmail.com>
2025-09-09 19:58:48 +0200

    feat: create repository

---
+++ src/test/java/org/shop/traffic/TrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shop.traffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
