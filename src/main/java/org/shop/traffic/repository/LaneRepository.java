605714c434309b1ea3c59dcca72a5a5ad935d711
Amelia <nawrotamelia3@gmail.com>
2025-09-25 19:27:14 +0200

    feat: delete unused repositories

--- src/main/java/org/shop/traffic/repository/LaneRepository.java
+++
@@ -1,9 +0,0 @@
-package org.shop.traffic.repository;
-
-import org.shop.traffic.model.Lane;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface LaneRepository extends JpaRepository<Lane, UUID> {
-}
--- src/main/java/org/shop/traffic/repository/TrafficLightRepository.java
+++
@@ -1,9 +0,0 @@
-package org.shop.traffic.repository;
-
-import org.shop.traffic.model.TrafficLight;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface TrafficLightRepository extends JpaRepository<TrafficLight, UUID> {
-}
