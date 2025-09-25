58fca298d549f4aadb422fc64a020b6ad16668be
Amelia <nawrotamelia3@gmail.com>
2025-09-25 19:34:50 +0200

    feat: split into two controllers

---
+++ src/main/java/org/shop/traffic/controller/IntersectionController.java
@@ -0,0 +1,42 @@
+package org.shop.traffic.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.shop.traffic.dto.request.CreateIntersectionRequest;
+import org.shop.traffic.dto.request.VehicleRequest;
+import org.shop.traffic.dto.response.IntersectionResponse;
+import org.shop.traffic.dto.response.VehicleResponse;
+import org.shop.traffic.service.TrafficSimulationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/intersection")
+@RequiredArgsConstructor
+public class IntersectionController {
+
+    private final TrafficSimulationService trafficSimulationService;
+
+    /** Handles POST requests to create a new traffic intersection **/
+    @PostMapping
+    public ResponseEntity<IntersectionResponse> createIntersection(
+            @RequestBody CreateIntersectionRequest request) {
+        IntersectionResponse response = trafficSimulationService.createTrafficIntersection(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+    /** Handles GET requests to retrieve the current state of a traffic intersection by its ID.**/
+    @GetMapping("/{id}")
+    public ResponseEntity<IntersectionResponse> getIntersection(@PathVariable UUID id){
+        IntersectionResponse response = trafficSimulationService.getTrafficIntersection(id);
+        return ResponseEntity.ok(response);
+    }
+    /** Handles POST requests to add a vehicle to a specific intersection **/
+    @PostMapping("/{id}/vehicle")
+    public ResponseEntity<VehicleResponse> addVehicleToIntersection(
+            @RequestBody VehicleRequest request, @PathVariable UUID id){
+        VehicleResponse response = trafficSimulationService.addVehicle(request, id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
--- src/main/java/org/shop/traffic/controller/TrafficSimulationController.java
+++ src/main/java/org/shop/traffic/controller/TrafficSimulationController.java
@@ -3,15 +3,11 @@ package org.shop.traffic.controller;
 import lombok.RequiredArgsConstructor;
 import org.shop.traffic.dto.request.*;
 import org.shop.traffic.dto.response.CycleResponse;
-import org.shop.traffic.dto.response.IntersectionResponse;
 import org.shop.traffic.dto.response.SimulationResponse;
-import org.shop.traffic.dto.response.VehicleResponse;
 import org.shop.traffic.service.TrafficSimulationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/traffic-simulation")
@@ -20,29 +16,8 @@ public class TrafficSimulationController {
 
     private final TrafficSimulationService trafficSimulationService;
 
-    /** Handles POST requests to create a new traffic intersection **/
-    @PostMapping("/intersection")
-    public ResponseEntity<IntersectionResponse> createIntersection(
-           @RequestBody CreateIntersectionRequest request) {
-        IntersectionResponse response = trafficSimulationService.createTrafficIntersection(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
-    /** Handles GET requests to retrieve the current state of a traffic intersection by its ID.**/
-    @GetMapping("/intersection/{id}")
-    public ResponseEntity<IntersectionResponse> getIntersection(@PathVariable UUID id){
-        IntersectionResponse response = trafficSimulationService.getTrafficIntersection(id);
-        return ResponseEntity.ok(response);
-    }
-    /** Handles POST requests to add a vehicle to a specific intersection **/
-    @PostMapping("/intersection/{id}/vehicle")
-    public ResponseEntity<VehicleResponse> addVehicleToIntersection(
-            @RequestBody VehicleRequest request, @PathVariable UUID id){
-        VehicleResponse response = trafficSimulationService.addVehicle(request, id);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
-
     /** Runs a full simulation based on a JSON file with commands. **/
-    @PostMapping("/simulation/run")
+    @PostMapping("/run")
     public ResponseEntity<SimulationResponse> runSimulation(
             @RequestBody SimulationRequest request){
         SimulationResponse response = trafficSimulationService.runSimulation(request);
